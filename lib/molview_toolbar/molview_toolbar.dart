49a484fc9e33c32d16ea9a3afc22342823fd9514
Herman Bergwerf <hermanbergwerf@gmail.com>
2016-04-13 16:02:29 +0200

    Add fancy search input design!

--- lib/molview_toolbar/molview_toolbar.dart
+++ lib/molview_toolbar/molview_toolbar.dart
@@ -5,6 +5,8 @@
 @HtmlImport('molview_toolbar.html')
 library molview_web.molview_toolbar;
 
+import 'dart:html';
+
 import 'package:polymer/polymer.dart';
 import 'package:web_components/web_components.dart' show HtmlImport;
 
@@ -17,13 +19,39 @@ import 'package:polymer_elements/iron_image.dart';
 // ignore: UNUSED_IMPORT
 import 'package:polymer_elements/iron_icons.dart';
 // ignore: UNUSED_IMPORT
-import 'package:polymer_elements/iron_icon.dart';
-// ignore: UNUSED_IMPORT
 import 'package:polymer_elements/paper_icon_button.dart';
 // ignore: UNUSED_IMPORT
 import 'package:polymer_elements/paper_tabs.dart';
+// ignore: UNUSED_IMPORT
+import 'package:polymer_elements/paper_material.dart';
 
+/// TODO: fix #search-input blur when clicking inside the #search div.
 @PolymerRegister('molview-toolbar')
 class MolViewToolbar extends PolymerElement {
   MolViewToolbar.created() : super.created();
+
+  /// Store if the search bar is focussed.
+  bool _searchFocus = false;
+
+  @Listen('search.tap')
+  void onSearchTap(Event event, Map detail) {
+    if (!_searchFocus) {
+      $$("#search-input").focus();
+      _searchFocus = true;
+    }
+  }
+
+  @Listen('search-input.focus')
+  void onSearchInputFocus(Event event, Map detail) {
+    $$("#search").classes.add('focussed');
+    $$("#search-ripple").classes.add('down');
+    _searchFocus = true;
+  }
+
+  @Listen('search-input.blur')
+  void onSearchInputBlur(Event event, Map detail) {
+    $$("#search").classes.remove('focussed');
+    $$("#search-ripple").classes.remove('down');
+    _searchFocus = false;
+  }
 }
