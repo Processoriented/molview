e8acee4b8425f214b5cd197e8074833ef8452c0d
Herman Bergwerf <hermanbergwerf@gmail.com>
2016-10-17 17:53:28 +0200

    Refactor omnibus out

--- lib/molview_web/molview_web.dart
+++ lib/molview_web/molview_web.dart
@@ -8,11 +8,6 @@ library molview_web.molview_web;
 import 'dart:html';
 import 'dart:async';
 
-import 'package:omnibus/omnibus.dart';
-//import 'package:molview_pubchem/molview_pubchem.dart';
-import 'package:molview_wikidata/molview_wikidata.dart';
-import 'package:molview_messages/molview_messages_v0.dart' as v0;
-
 // ignore: UNUSED_IMPORT
 import 'package:web_components/web_components.dart' show HtmlImport;
 // ignore: UNUSED_IMPORT
@@ -39,9 +34,6 @@ import 'package:molview_web/molview_search_input/molview_search_input.dart';
 /// TODO: isolate GUI management and actual event handling.
 @PolymerRegister('molview-web')
 class MolViewWeb extends PolymerElement {
-  /// Message bus
-  Omnibus _bus;
-
   /// Inner search input
   MolViewSearchInput _input;
 
@@ -69,24 +61,13 @@ class MolViewWeb extends PolymerElement {
 
       // Add callback for search input change.
       _input.onValueChange((Event event) async {
-        // Use message bus to find search suggesions.
-        if (_bus != null) {
-          // Only proceed if the value is longer than the threshold number.
+        // Only proceed if the value is longer than the threshold number.
           var value = _input.value;
           if (value.length > autocompleteThreshold) {
-            // Get suggestions using a FindSearchSuggestions request.
-            var responses = await _bus.publishRequest(
-                new v0.FindSearchSuggestions(
-                    value, 'en', [wikidataAutocpProvider], 5));
-            var stream = v0.streamSearchSuggestions(responses);
-
-            // Add all suggestions to the suggesions list.
-            var suggestions = new List<String>();
-            await for (var response in stream) {
-              suggestions.addAll(new List<String>.generate(
-                  response.suggestions.length,
-                  (int i) => response.suggestions[i].title));
-            }
+            // Get suggestions.
+            // TODO: reimplement Wikidata lookup in data library.
+            //final suggestions = wikidata.loadSuggestions('en', 5);
+            final suggestions = ['No results'];
 
             // Load new suggestions into input popup.
             // Check if the value has changed since.
@@ -97,14 +78,8 @@ class MolViewWeb extends PolymerElement {
             // Set the autocomplete popup to the default content.
             _input.setSuggestions(defaultSuggestions);
           }
-        }
       });
     });
-  }
-
-  /// Set message bus
-  void setMessagesBus(Omnibus bus) {
-    _bus = bus;
   }
 
   /// Toggle drawer when the custom 'drawer-toggle' event is triggered.
